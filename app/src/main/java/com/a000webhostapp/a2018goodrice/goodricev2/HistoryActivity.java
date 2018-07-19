5619d57b59573d4f8335dc5fbf3e7242a0a5f6f7
8236fn <40617124+8236fn@users.noreply.github.com>
2018-07-19 11:56:46 +0800

    20180719-2
    整合layout

---
+++ app/src/main/java/com/a000webhostapp/a2018goodrice/goodricev2/HistoryActivity.java
@@ -0,0 +1,13 @@
+package com.a000webhostapp.a2018goodrice.goodricev2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
